3445ac705340af1ad7104e3771cd6ff877822a44
Srinivas Rao <srinivas.rao@kerv.com>
2026-03-17 20:05:11 +0530

    Added reporting service

---
+++ CompareDataTool.Domain/Services/ReportingService.cs
@@ -0,0 +1,22 @@
+﻿using CompareDataTool.Domain.Interfaces;
+using CompareDataTool.Domain.Models;
+
+namespace CompareDataTool.Domain.Services
+{
+    public class ReportingService
+    {
+        private readonly IAppDataRepository appDataRepository;
+        private readonly AppConfiguration appConfiguration;
+
+        public ReportingService(IAppDataRepository appDataRepository, AppConfiguration appConfiguration)
+        {
+            this.appConfiguration = appConfiguration;
+            this.appDataRepository = appDataRepository;
+        }
+
+        public async Task GenerateReport(string runId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
